8bd16d240a0db52fb04fb36bc3935c4353c827a0
khanhtranlc99 <56682123+khanhtranlc99@users.noreply.github.com>
2024-06-19 17:10:24 +0700

    NewGroundTile
    NewGroundTile

--- Assets/Base/00_BaseCode/Scripts/BoltBase.cs
+++ Assets/Base/00_BaseCode/Scripts/BoltBase.cs
@@ -2,7 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using Spine.Unity;
+using UnityEngine.UI;
 public class BoltBase : MonoBehaviour
 {
-  
+    public List<SkeletonAnimation> skeletonAnimation;
+    public Text tvState;
+    public void Start()
+    {
+   
+    }
+    [Button]
+    private void PlayIdle()
+    {
+        foreach (var item in skeletonAnimation)
+        {
+            item.SetAnimation("idle", true);
+        }
+        tvState.text = "idle";
+    }
+    [Button]
+    private void PlayMove()
+    {
+        foreach (var item in skeletonAnimation)
+        {
+            item.SetAnimation("move", true);
+        }
+        tvState.text = "move";
+    }
+    [Button]
+    private void PlayDie()
+    {
+        foreach (var item in skeletonAnimation)
+        {
+            item.SetAnimation("die", true);
+        }
+        tvState.text = "die";
+    }
 }
